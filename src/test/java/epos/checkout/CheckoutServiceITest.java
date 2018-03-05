53eacdf021888721616f1c8178dc64e1eb1e8dae
alastair <alastair.knowles@tide.co>
2018-03-05 21:01:41 +0000

    Missing failure assertion

--- src/test/java/epos/checkout/CheckoutServiceITest.java
+++ src/test/java/epos/checkout/CheckoutServiceITest.java
@@ -1,12 +1,13 @@
 package epos.checkout;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CheckoutServiceITest {
 
     @Test
     public void shouldCalculateTotal() {
-
+        Assert.fail();
     }
 
 }
